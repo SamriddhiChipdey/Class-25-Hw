4a5c2d2fb62f553e0cf2c140a3aa54d63ab77ea4
SamriddhiChipdey <82385750+SamriddhiChipdey@users.noreply.github.com>
2021-05-21 17:22:39 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,23 @@
+class ground {
+    constructor(x,y,width,height)   {
+        var options = {
+           isStatic:true 
+        }
+    this.body = Bodies.rectangle(x,y,width,height,options);
+    this.width = width;
+    this.height = height;
+    //this.image = loadImage('base.png');//
+    World.add(world,this.body);
+}
+
+    display()   {
+        ground1.shapeColor="black";
+        push();
+        var pos1 = this.body.position;
+        translate(pos1.x,pos1.y);
+        imageMode(CENTER);
+        image(this.image,0,0,this.width,this.height);
+        pop();
+    }
+
+}
